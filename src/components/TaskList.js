dd270df5c5244c9e99bb0a1a8d297195c8c75010
Dominik Mačković <dominik.mackovic@bizzon.com>
2022-03-01 12:51:45 +0100

    Dodan stories i testovi

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,23 +1,54 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Task from './Task';
 
 export default function TaskList(listArgs) {
   const {loading, tasks, onPinTask, onArchiveTask} = listArgs;
   const events = {onPinTask, onArchiveTask};
 
+  const LoadingRow = (
+    <div className="loading-item">
+      <span className="glow-checkbox" />
+      <span className="glow-text">
+        <span>Loading</span> <span>cool</span> <span>state</span>
+      </span>
+    </div>
+  );
+
   if (loading) {
-    return <div className='list-items'>loading</div>;
+    return (
+      <div className="list-items" data-testid="loading" key={"loading"}>
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+        {LoadingRow}
+      </div>
+    );
   }
 
-  if (tasks.length === 0) {
-    return <div className='list-items'>empty</div>;
-  }
+  const tasksInOrder = [
+   ...tasks.filter((t) => t.state === "TASK_PINNED"),
+   ...tasks.filter((t) => t.state !== "TASK_PINNED"),
+ ];
 
   return (
     <div className='list-items'>
-      {tasks.map(task => (
+      {tasksInOrder.map(task => (
         <Task key={task.id} task={task} {...events} />
       ))}
     </div>
   );
 }
+
+TaskList.propTypes = {
+  loading: PropTypes.bool,
+  tasks: PropTypes.arrayOf(Task.propTypes.task).isRequired,
+  onPinTask: PropTypes.func,
+  onArchiveTask: PropTypes.func,
+};
+
+TaskList.defaultProps = {
+  loading: false
+};
